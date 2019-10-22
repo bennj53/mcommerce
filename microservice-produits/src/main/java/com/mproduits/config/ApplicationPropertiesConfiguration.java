7981ce02013b007e438e0b269242d802d8e9a214
bennj53 <benoitjulliot@gmail.com>
2019-10-22 08:03:56 +0200

    add proxy paiement et commande

--- microservice-produits/src/main/java/com/mproduits/config/ApplicationPropertiesConfiguration.java
+++ microservice-produits/src/main/java/com/mproduits/config/ApplicationPropertiesConfiguration.java
@@ -3,12 +3,14 @@ package com.mproduits.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @Component
 @ConfigurationProperties("mes.configs")
+@RefreshScope
 public class ApplicationPropertiesConfiguration {
     private int limitDeProduit;
 
